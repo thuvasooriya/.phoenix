5d41cf3322d81b35a20361b07eb243c4f5834e7f
thuvasooriya <thuvaragan28@gmail.com>
2024-11-20 09:18:32 +0530

    skhd

--- modules/darwin/yabai.nix
+++ modules/darwin/yabai.nix
@@ -2,5 +2,46 @@
   services.yabai = {
     enable = true;
     enableScriptingAddition = true;
+    config = {
+      focus_follows_mouse = "autoraise";
+      mouse_follows_focus = "off";
+      window_placement = "second_child";
+      # window_opacity = "off";
+      # window_opacity_duration = "0.0";
+      window_border = "on";
+      window_border_placement = "inset";
+      # window_border_width = 2;
+      # window_border_radius = 3;
+      active_window_border_topmost = "off";
+      window_topmost = "on";
+      window_shadow = "float";
+      # active_window_border_color = "0xff5c7e81";
+      # normal_window_border_color = "0xff505050";
+      # insert_window_border_color = "0xffd75f5f";
+      active_window_opacity = "1.0";
+      normal_window_opacity = "1.0";
+      split_ratio = "0.50";
+      auto_balance = "on";
+      mouse_modifier = "fn";
+      mouse_action1 = "move";
+      mouse_action2 = "resize";
+      layout = "bsp";
+      top_padding = 8;
+      bottom_padding = 8;
+      left_padding = 8;
+      right_padding = 8;
+      window_gap = 8;
+    };
+
+    extraConfig = ''
+      yabai -m signal --add event=dock_did_restart action="sudo yabai --load-sa"
+      sudo yabai --load-sa
+      # rules
+      yabai -m rule --add app="^(LuLu|Calculator|Software Update|Dictionary|System Preferences|System Settings|zoom.us|Photo Booth|Archive Utility|Python|App Store|Raycast|Activity Monitor)$" manage=off
+      yabai -m rule --add label="Finder" app="^Finder$" title="(Co(py|nnect)|Move|Info|Pref)" manage=off
+      yabai -m rule --add label="Safari" app="^Safari$" title="^(General|(Tab|Password|Website|Extension)s|AutoFill|Se(arch|curity)|Privacy|Advance)$" manage=off
+      yabai -m rule --add label="About This Mac" app="System Information" title="About This Mac" manage=off
+
+    '';
   };
 }
