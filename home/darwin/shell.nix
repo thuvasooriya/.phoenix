32bc69191ec4abcfb5cfd0e00bbcbca6a2730263
thuvasooriya <thuvaragan28@gmail.com>
2025-01-08 10:47:39 +0530

    calm before the storm

--- home/darwin/shell.nix
+++ home/darwin/shell.nix
@@ -6,9 +6,7 @@
   programs = {
     fish = {
       loginShellInit = ''
-        if [[ $(uname -m) == 'arm64' ]]; then
-            eval "$(/opt/homebrew/bin/brew shellenv)"
-        fi
+        eval "$(/opt/homebrew/bin/brew shellenv)"
       '';
     };
   };
